abd8f6c72a9df468f31550563e338fbc5a5a8403
cheernenimahesh <110657457+maheshcheerneni143@users.noreply.github.com>
2023-11-28 13:24:07 +0530

    Update settings.py

--- Amzon/settings.py
+++ Amzon/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-)$xim7k-7gsgpmnk%0qic=!e8eiss#)z((e1k!@pjim$%z!%dd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
